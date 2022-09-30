d3f16a4801edf715cea7a81201fe3f76c0cbbc53
Abraham Daguer <abrahamg1235@gmail.com>
2022-09-30 07:25:08 -0500

    oscilations v2

--- Assets/06 Oscilations/Scripts/Graph.cs
+++ Assets/06 Oscilations/Scripts/Graph.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+   
+    [SerializeField] int totalPoints = 10;
+    [SerializeField] float distanceFactor = 1;
+    [SerializeField] float amplitud = 4;
+
+    [SerializeField] GameObject prefab;
+    [SerializeField] LineRenderer LineRenderer;
+
+
+    private GameObject[] allPoints; 
     void Start()
     {
-        
+        allPoints = new GameObject[totalPoints];
+        for(int i = 0; i < totalPoints; i++)
+        {
+            allPoints[i] = Instantiate(prefab, transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < allPoints.Length; i++)
+        {
+           
+            float x = i * distanceFactor;
+            float y = amplitud * Mathf.Sin(x * Time.time);
+            allPoints[i].transform.localPosition = new Vector3(x, y, 0);
+        }
     }
 }
